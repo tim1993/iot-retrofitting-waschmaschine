fe912aff50053b655b4cfcfbb83005eb6e558b43
Dominik <dominik.deschner@monumental-it.de>
2022-02-21 21:27:57 +0100

    Adds initial content

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Device.Spi;
+using System.Numerics;
+using Iot.Device.Adxl345;
+
+Console.WriteLine("Hello, World!");
+
+SpiConnectionSettings settings = new SpiConnectionSettings(0, 0)
+{
+    ClockFrequency = Adxl345.SpiClockFrequency,
+    Mode = Adxl345.SpiMode
+};
+
+var device = SpiDevice.Create(settings);
+
+using (Adxl345 sensor = new Adxl345(device, GravityRange.Range08))
+{
+    // loop
+    while (true)
+    {
+        // read data
+        Vector3 data = sensor.Acceleration;
+
+        Console.WriteLine($"X: {data.X.ToString("0.00")} g");
+        Console.WriteLine($"Y: {data.Y.ToString("0.00")} g");
+        Console.WriteLine($"Z: {data.Z.ToString("0.00")} g");
+        Console.WriteLine();
+
+        // wait for 500ms
+        Thread.Sleep(1000);
+    }
+}
---
+++ scripts/publish-ssh.ps1
@@ -0,0 +1,13 @@
+$serverAddress = "pi@192.168.0.249"
+
+$path = (Get-Item "$PWD").FullName 
+$folderName = (Get-Item $path).Name
+
+Write-Host $path
+
+Remove-Item -Recurse -Path bin/ -ErrorAction Ignore
+
+dotnet publish -o out
+
+ssh $serverAddress "mkdir -p /home/pi/${folderName}"
+scp -r $path/out/** "${serverAddress}:/home/pi/${folderName}"
