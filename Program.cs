9db60dfce9d6560aa086700acabbcae2c7d4fac1
Dominik <dominik.deschner@monumental-it.de>
2022-02-21 22:12:30 +0100

    Adds sensor infrastructure

--- Program.cs
+++ Program.cs
@@ -2,8 +2,9 @@
 using System.Device.Spi;
 using System.Numerics;
 using Iot.Device.Adxl345;
-
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WashingIot.Data;
 
 SpiConnectionSettings settings = new SpiConnectionSettings(0, 0)
 {
@@ -15,18 +16,18 @@ var device = SpiDevice.Create(settings);
 
 using (Adxl345 sensor = new Adxl345(device, GravityRange.Range08))
 {
-    // loop
-    while (true)
-    {
-        // read data
-        Vector3 data = sensor.Acceleration;
 
-        Console.WriteLine($"X: {data.X.ToString("0.00")} g");
-        Console.WriteLine($"Y: {data.Y.ToString("0.00")} g");
-        Console.WriteLine($"Z: {data.Z.ToString("0.00")} g");
-        Console.WriteLine();
-
-        // wait for 500ms
-        Thread.Sleep(1000);
-    }
+    var dataCollector = new AsyncSensorDataCollector<Adxl345Reading>(TimeSpan.FromMilliseconds(100), new Adxl345DataSource(sensor));
+    dataCollector.Start();
+    Console.ReadLine();
 }
+
+
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+
+        });
